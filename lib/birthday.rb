a9a4302964c7850a27558ea566729b0cb4969e29
Francisco Costa <francisco@fcosta.pt>
2019-03-18 14:11:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,10 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age < 13
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids each {|name, age| if age < 13 then puts "Happy Birthday #{name}! You are now #{age} years old!" end}
 end
